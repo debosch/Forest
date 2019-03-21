76f78579f610044a028f10e9d3354544a423595b
debosch <vlad.shiltsin@gmail.com>
2019-03-21 22:39:27 +0200

    refactor

---
+++ ForestGame/Assets/Scripts/IgnoreCollision.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class IgnoreCollision : MonoBehaviour
+{
+    [SerializeField]
+    private Collider2D target;
+
+    private void Awake()
+    {
+        Physics2D.IgnoreCollision(GetComponent<Collider2D>(), target, true);
+    }
+}
